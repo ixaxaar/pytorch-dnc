add63b406df2cc52a686f75c87e6029da7bce392
ixaxaar <root@ixaxaar.in>
2017-10-27 17:04:12 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='dnc',
 
-    version='0.0.2',
+    version='0.0.3',
 
     description='Differentiable Neural Computer, for Pytorch',
     long_description=long_description,
