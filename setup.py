d336eba1dfc2d5cfe2256b0e387afe55005db4df
ixaxaar <root@ixaxaar.in>
2025-05-23 16:04:36 +0530

    Release 2.0 (#66)
    * bump version and release
    
    * bump version and release

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -16,43 +17,39 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='dnc',
-    version='1.1.0',
-    description='Differentiable Neural Computer, for Pytorch',
+    name="dnc",
+    version="2.0.0b1",
+    description="Differentiable Neural Computer, for Pytorch",
     long_description=long_description,
-
     # The project's main homepage.
-    url='https://github.com/pypa/dnc',
-
+    url="https://github.com/pypa/dnc",
     # Author details
-    author='Russi Chatterjee',
-    author_email='root@ixaxaar.in',
-
+    author="Russi Chatterjee",
+    author_email="root@ixaxaar.in",
     # Choose your license
-    license='MIT',
-
+    license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    keywords='differentiable neural computer dnc memory network',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'tasks', 'scripts']),
-    install_requires=['torch', 'numpy', 'flann'],
+    keywords="differentiable neural computer dnc memory network",
+    packages=find_packages(exclude=["contrib", "docs", "tests", "tasks", "scripts"]),
+    install_requires=["torch", "numpy", "flann"],
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
     },
-    python_requires='>=3',
+    python_requires=">=3",
 )
